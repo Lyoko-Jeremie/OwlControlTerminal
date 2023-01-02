dd44bf68caefbf58c023580557806667690b5665
LyokoJeremie <lucheng989898@gmail.com>
2023-01-02 12:42:11 +0800

    style

--- app/src/main/java/moe/jeremie/owl/terminal/MainActivity.kt
+++ app/src/main/java/moe/jeremie/owl/terminal/MainActivity.kt
@@ -3,11 +3,15 @@ package moe.jeremie.owl.terminal
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.databinding.DataBindingUtil
 import moe.jeremie.owl.terminal.ImageProtocol.ImageProtocolKotlin.ImageProtocol.ImageRequest
+import moe.jeremie.owl.terminal.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private val TAG = "MainActivity"
+
+    private lateinit var binding: ActivityMainBinding
 
     override fun onDestroy() {
         super.onDestroy()
@@ -19,8 +23,20 @@ class MainActivity : AppCompatActivity() {
 
         Log.v(TAG, "onCreate")
 
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
         supportActionBar?.hide()
+
+
+//        binding.bBack.background.alpha = 128
+//        binding.bFront.background.alpha = 128
+//        binding.bLeft.background.alpha = 128
+//        binding.bRight.background.alpha = 128
+//        binding.bUp.background.alpha = 128
+//        binding.bDown.background.alpha = 128
+//        binding.bCw.background.alpha = 128
+//        binding.bCcw.background.alpha = 128
 
 //        var a = ImageRequest.newBuilder()
 //            .setCameraId(1)
